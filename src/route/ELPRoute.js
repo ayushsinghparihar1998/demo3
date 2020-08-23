e7b7f550c37cee051ebee667d328d19d9ebd591a
nehakochar <neha.kochar@lmsin.com>
2020-08-23 16:40:39 +0530

    correct import

--- src/route/ELPRoute.js
+++ src/route/ELPRoute.js
@@ -1,33 +1,32 @@
-import React, { Component } from "react";
-import { Switch } from "react-router-dom";
-import Landing from "../components/core/landing";
-import Login from "../components/login/login";
-import ProfessionalLogin from "../components/login/professionalLogin";
-import Becomelistener from "../components/signup/becomeListener";
-import Listenersignup from "../components/signup/listenerSignup";
-import Usersignup from "../components/signup/userSignup";
-import Userdashboard from "../components/listener/userdashboard";
-import Chat from "../components/listener/chat";
-import PrivateRouteList from "./PrivateRouteList";
-import PrivateRouteProff from "./PrivateRouteProff";
-import PublicRoute from "./PublicRoute";
-import UserdashboardProff from "../components/professional/userdashboardProff";
-import ChatProff from "../components/professional/chatproff";
-import UserdashboardCust from "../components/user/userdashboard";
-import PrivateRouteUser from "./PrivateRouteUser";
-import PrivateRoutes from "./PrivateRoutes";
-import MyProfile from "../components/editprofile/myprofile";
-import EditProfile from "../components/editprofile/editprofile";
-import ChatUser from "../components/user/chatUser";
-import Resetpassword from "../components/login/resetpassword";
-import Forgotpassword from "../components/login/forgotpassword";
-import Userotp from "../components/login/userotp";
-import AdminLogin from "../components/admin/adminlogin";
-import Adminlistener from "../components/admin/listener";
+import React, { Component } from 'react';
+import { Switch } from 'react-router-dom';
+import Landing from '../components/core/landing';
+import Login from '../components/login/Login';
+import ProfessionalLogin from '../components/login/professionalLogin';
+import Becomelistener from '../components/signup/becomeListener';
+import Listenersignup from '../components/signup/listenerSignup';
+import Usersignup from '../components/signup/userSignup';
+import Userdashboard from '../components/listener/userdashboard';
+import Chat from '../components/listener/chat';
+import PrivateRouteList from './PrivateRouteList';
+import PrivateRouteProff from './PrivateRouteProff';
+import PublicRoute from './PublicRoute';
+import UserdashboardProff from '../components/professional/userdashboardProff';
+import ChatProff from '../components/professional/chatproff';
+import UserdashboardCust from '../components/user/userdashboard';
+import PrivateRouteUser from './PrivateRouteUser';
+import PrivateRoutes from './PrivateRoutes';
+import MyProfile from '../components/editprofile/myprofile';
+import EditProfile from '../components/editprofile/editprofile';
+import ChatUser from '../components/user/chatUser';
+import Resetpassword from '../components/login/resetpassword';
+import Forgotpassword from '../components/login/forgotpassword';
+import Userotp from '../components/login/userotp';
+import AdminLogin from '../components/admin/adminlogin';
+import Adminlistener from '../components/admin/listener';
 
 class ELPRoute extends Component {
   render() {
-
     return (
       <Switch>
         <PublicRoute exact path="/" component={Landing} />
