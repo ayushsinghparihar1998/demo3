b210b12427bd40f14273b0f181674b4879ef3d00
Dharmpal123 <63254582+Dharmpal123@users.noreply.github.com>
2020-09-18 20:09:47 +0530

    scss

--- src/components/userdashboard/userdashboard.js
+++ src/components/userdashboard/userdashboard.js
@@ -63,14 +63,14 @@ class Userdashboard extends Component {
       this.setState({
         sucess: true,
         result: true,
-        message: "your score is " + result + "%",
+        message: "your score is " + " " + result + "%",
       });
     } else {
       this.setState({
         sucess: true,
         result: false,
-        message: "your score is " + result + "%",
-      });
+        message: "your score is " + " " + result + "%",
+      }); 
     }
 
     socket.on("connect", function () {
@@ -665,6 +665,6 @@ class Userdashboard extends Component {
 export default connect(null, {
   actionGetRecentJoin,
   actionGetListnerDashBoard,
-})(Userdashboard);
+})(Userdashboard); 
 
 
