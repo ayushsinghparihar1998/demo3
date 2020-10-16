a6096bbf42e204c5e507a99f546b737057a37a0e
Dharmpal123 <63254582+Dharmpal123@users.noreply.github.com>
2020-10-16 20:26:00 +0530

    media.js and ngo.scss files

--- src/components/core/media.js
+++ src/components/core/media.js
@@ -2,34 +2,40 @@ import React, { Component } from "react";
 import { Button, NavDropdown, Carousel, Container, Row, Col, Image, Form, Tabs, Tab } from "react-bootstrap";
 import NavBar from "../core/nav";
 import Footer from "../core/footer";
-import Requestuser from "../../assets/images/pro_img.svg";
+import Sharebtnblue from "../../assets/images/sharebtnblue.svg";
 import Ngoone from "../../assets/images/ngo1.svg";
 import Ngotwo from "../../assets/images/ngo2.svg";
-import Ngothree from "../../assets/images/ngo3.svg";   
+import Ngothree from "../../assets/images/ngo3.svg";    
 
 class Media extends Component { 
     render() {
         return (
-            <div className="page__wrapper innerpage">      
+            <div className="page__wrapper innerpage">        
                 <div className="main_baner">
                     <NavBar {...this.props} />
                 </div> 
-                <div className="ngo_services">
+                <div className="ngo_services media_details"> 
                     <Container>
-                        <div className="ngo_listing  mt-4 mb-4">   
-                            <div className="col14 fw600 fs22 text-center w-100 mt-4 pt-3">Media</div>  
-                            <hr className="ngohr" />  
-
+                        <div className="ngo_listing  mt-4 mb-4"> 
                             <Row className="mt-4">        
                                 <Col lg={4} md={4} sm={12}>   
-                                    <div className="ngo_social">   
+                                    <div className="ngo_social">     
                                         <Image src={Ngoone} alt="" className="w-100" />  
                                         <div className="p-3">  
-                                            <div className="col1 fs18 fw600">Child and Social Welfare Society</div>
-                                            <div className="col14 fs14 fw400 pt-1">Since 1970</div>
-                                            <hr className="social_hr" />
+                                            <div className="media_list mb-3"> 
+                                                <div className="col14 fs14 fw400 pt-1">Posted on April 15, 2020 </div>
+                                                <div className="col14 fs14 fw400 pt-1">220 views</div> 
+                                            </div>
+                                            <div className="col1 fs18 fw600 mb-3">Child and Social Welfare Society</div>
+                                            
                                             <div className="fs14 col29 fw300">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</div> 
-                                            <Button className="btnType19">Read More</Button>
+                                            <hr className="social_hr" />
+                                            <div className="media_list">
+                                                <div className="col10 fs14 fw600 pointer">http:socialwelfare.com</div>
+                                                <div className="position-relative">
+                                                    <Image src={Sharebtnblue} alt="Sharebtnblues" className="pointer" />  
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </Col> 
@@ -37,11 +43,20 @@ class Media extends Component {
                                     <div className="ngo_social">   
                                         <Image src={Ngotwo} alt="" className="w-100" />  
                                         <div className="p-3">  
-                                            <div className="col1 fs18 fw600">Young Generation</div>
-                                            <div className="col14 fs14 fw400 pt-1">Since 1970</div>
-                                            <hr className="social_hr" />
+                                            <div className="media_list mb-3"> 
+                                                <div className="col14 fs14 fw400 pt-1">Posted on April 15, 2020 </div>
+                                                <div className="col14 fs14 fw400 pt-1">220 views</div> 
+                                            </div>
+                                            <div className="col1 fs18 fw600 mb-3">Child and Social Welfare Society</div>
+                                            
                                             <div className="fs14 col29 fw300">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</div> 
-                                            <Button className="btnType19">Read More</Button>  
+                                            <hr className="social_hr" />
+                                            <div className="media_list">
+                                                <div className="col10 fs14 fw600 pointer">http:socialwelfare.com</div>
+                                                <div className="position-relative">
+                                                    <Image src={Sharebtnblue} alt="Sharebtnblues" className="pointer" />  
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </Col>
@@ -49,11 +64,20 @@ class Media extends Component {
                                     <div className="ngo_social">   
                                         <Image src={Ngothree} alt="" className="w-100" />  
                                         <div className="p-3">  
-                                            <div className="col1 fs18 fw600">Humanity Association</div>
-                                            <div className="col14 fs14 fw400 pt-1">Since 1970</div>
-                                            <hr className="social_hr" />
+                                            <div className="media_list mb-3"> 
+                                                <div className="col14 fs14 fw400 pt-1">Posted on April 15, 2020 </div>
+                                                <div className="col14 fs14 fw400 pt-1">220 views</div> 
+                                            </div>
+                                            <div className="col1 fs18 fw600 mb-3">Child and Social Welfare Society</div>
+                                            
                                             <div className="fs14 col29 fw300">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</div> 
-                                            <Button className="btnType19">Read More</Button> 
+                                            <hr className="social_hr" />
+                                            <div className="media_list">
+                                                <div className="col10 fs14 fw600 pointer">http:socialwelfare.com</div>
+                                                <div className="position-relative">
+                                                    <Image src={Sharebtnblue} alt="Sharebtnblues" className="pointer" />  
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </Col>
@@ -61,11 +85,20 @@ class Media extends Component {
                                     <div className="ngo_social">   
                                         <Image src={Ngoone} alt="" className="w-100" />  
                                         <div className="p-3">  
-                                            <div className="col1 fs18 fw600">Child and Social Welfare Society</div>
-                                            <div className="col14 fs14 fw400 pt-1">Since 1970</div>
-                                            <hr className="social_hr" />
+                                            <div className="media_list mb-3"> 
+                                                <div className="col14 fs14 fw400 pt-1">Posted on April 15, 2020 </div>
+                                                <div className="col14 fs14 fw400 pt-1">220 views</div> 
+                                            </div>
+                                            <div className="col1 fs18 fw600 mb-3">Child and Social Welfare Society</div>
+                                            
                                             <div className="fs14 col29 fw300">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</div> 
-                                            <Button className="btnType19">Read More</Button>
+                                            <hr className="social_hr" />
+                                            <div className="media_list">
+                                                <div className="col10 fs14 fw600 pointer">http:socialwelfare.com</div>
+                                                <div className="position-relative">
+                                                    <Image src={Sharebtnblue} alt="Sharebtnblues" className="pointer" />  
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </Col>
@@ -73,11 +106,20 @@ class Media extends Component {
                                     <div className="ngo_social">   
                                         <Image src={Ngotwo} alt="" className="w-100" />  
                                         <div className="p-3">  
-                                            <div className="col1 fs18 fw600">Humanity Association</div>
-                                            <div className="col14 fs14 fw400 pt-1">Since 1970</div>
-                                            <hr className="social_hr" />
+                                            <div className="media_list mb-3"> 
+                                                <div className="col14 fs14 fw400 pt-1">Posted on April 15, 2020 </div>
+                                                <div className="col14 fs14 fw400 pt-1">220 views</div> 
+                                            </div>
+                                            <div className="col1 fs18 fw600 mb-3">Child and Social Welfare Society</div>
+                                            
                                             <div className="fs14 col29 fw300">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</div> 
-                                            <Button className="btnType19">Read More</Button>  
+                                            <hr className="social_hr" />
+                                            <div className="media_list">
+                                                <div className="col10 fs14 fw600 pointer">http:socialwelfare.com</div>
+                                                <div className="position-relative">
+                                                    <Image src={Sharebtnblue} alt="Sharebtnblues" className="pointer" />  
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </Col>
@@ -85,11 +127,20 @@ class Media extends Component {
                                     <div className="ngo_social">   
                                         <Image src={Ngothree} alt="" className="w-100" />  
                                         <div className="p-3">  
-                                            <div className="col1 fs18 fw600">Young Generation</div>
-                                            <div className="col14 fs14 fw400 pt-1">Since 1970</div>
-                                            <hr className="social_hr" />
+                                            <div className="media_list mb-3"> 
+                                                <div className="col14 fs14 fw400 pt-1">Posted on April 15, 2020 </div>
+                                                <div className="col14 fs14 fw400 pt-1">220 views</div> 
+                                            </div>
+                                            <div className="col1 fs18 fw600 mb-3">Child and Social Welfare Society</div>
+                                            
                                             <div className="fs14 col29 fw300">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</div> 
-                                            <Button className="btnType19">Read More</Button> 
+                                            <hr className="social_hr" />
+                                            <div className="media_list">
+                                                <div className="col10 fs14 fw600 pointer">http:socialwelfare.com</div>
+                                                <div className="position-relative">
+                                                    <Image src={Sharebtnblue} alt="Sharebtnblues" className="pointer" />  
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </Col>
