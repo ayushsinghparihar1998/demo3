0b10fea251e4959919e7bcee6372d4fd2506ab60
Abhipsa <abhipsa.hota@lmsin.com>
2020-12-16 20:39:26 +0530

    org

--- src/constant.js
+++ src/constant.js
@@ -4,8 +4,8 @@ export default {
     WEB_BASE_URL:'https://www.eatluvnpray.org/',
 
 
-    // SERVER_URL:'https://www.eatluvnpray.org/', //live 
-    SERVER_URL:'https://staging.eatluvnpray.org/', //staging  
+    SERVER_URL:'https://www.eatluvnpray.org/', //live 
+    // SERVER_URL:'https://staging.eatluvnpray.org/', //staging  
 
 
     SOCKET_URL:'https://eatluvnpray.org:8443' // live  
