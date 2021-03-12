866f1a3e87ace94a027192f9a41ca6304edde5b3
madhav-karambelkar <mady18.1998@gmail.com>
2021-03-12 18:51:12 +0530

    changed lengths in blogs

--- src/components/admin/professionalBlogPress.js
+++ src/components/admin/professionalBlogPress.js
@@ -343,20 +343,21 @@ class ProfessinalBlogPress extends Component {
                       <Form.Control
                         onChange={(e) =>
                           // this.setState({ title: e.target.value })
+                          // .replace(
+                          //   /[^a-zA-Z0-9 ]/g,
+                          //   ""
+                          // )
                           this.setState({
                             pblobj: {
                               ...this.state.pblobj,
-                              pbl_title: e.target.value.replace(
-                                /[^a-zA-Z0-9 ]/g,
-                                ""
-                              ),
+                              pbl_title: e.target.value,
                             },
                           })
                         }
                         type="text"
                         value={pblobj.pbl_title}
                         className="inputTyp2"
-                        maxLength={100}
+                        maxLength={150}
                       />
                       <div className="col27 fs14 fw400 mt-2 error">
                         {errors.title}
@@ -403,20 +404,21 @@ class ProfessinalBlogPress extends Component {
                       </Form.Label>
                       <Form.Control
                         onChange={(e) =>
+                          // .replace(
+                          //   /[^a-zA-Z ]/g,
+                          //   ""
+                          // )
                           this.setState({
                             pblobj: {
                               ...this.state.pblobj,
-                              pbl_written_by: e.target.value.replace(
-                                /[^a-zA-Z ]/g,
-                                ""
-                              ),
+                              pbl_written_by: e.target.value,
                             },
                           })
                         }
                         type="text"
                         value={pblobj.pbl_written_by}
                         className="inputTyp2"
-                        maxLength={100}
+                        maxLength={150}
                       />
                       <div className="col27 fs14 fw400 mt-2 error">
                         {errors.writtenby}
